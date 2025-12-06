971d1f33665c3ec2f25f85827760270a0960912a
Chiraz Ben Boubaker <99488895+ChirazBenBoubaker@users.noreply.github.com>
2025-12-06 12:13:25 +0100

    Update AccountResource.java

--- src/main/java/com/jhipster/itprogress/pfe/web/rest/AccountResource.java
+++ src/main/java/com/jhipster/itprogress/pfe/web/rest/AccountResource.java
@@ -19,7 +19,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.HtmlUtils;
-import jakarta.servlet.http.HttpServletRequest;
 
 
 /**
